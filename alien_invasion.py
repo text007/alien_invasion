c860e9fee931d1755e088bf6877a742a712d9685
unknown <jzheng@qumitech.local>
2019-10-24 15:29:06 +0800

    test

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,50 +1,46 @@
-import pygame   # pygame 模块包含开发游戏所需功能
-from pygame.sprite import Group
+import pygame
+import sys
 
 from settings import Settings
-from game_stats import GameeStats
+from game_stats import GameStats
 from ship import Ship
 from alien import Alien
-import  game_functions as gf    # 导入模块，指定一个别名gf
+import game_functions as gf
+from pygame.sprite import Group
 from button import Button
+from scoreboard import Scoreboard
 
-def run_game(): # 定义一个方法
-    # 初始化游戏并创建一个屏幕对象
-    pygame.init()   # 初始化背景设置，让 python 能够正常的工作
+
+def run_game():
+    pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))   # 创建一个游戏窗口
-    pygame.display.set_caption('Alien Invasion')
-
-    # 创建play按钮
-    play_button = Button(ai_settings, screen, 'play')
-
-    # 创建一艘飞船、一个子弹编组和一个外星人编组
-    ship = Ship(ai_settings, screen) # 创建一个实例
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    pygame.display.set_caption("Alien Invasion")
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+    # 创建一艘飞船、一个外星人、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-
     # 创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
+    # 创建一个用于存储游戏统计信息的实例,并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
-    # 设置背景颜色
-    bg_color = (ai_settings.bg_color)  # 设置背景颜色为浅灰色
-
-    # 创建一个外星人
-    alien = Alien(ai_settings, screen)
-
-    # 创建一个用于存储游戏统计信息的实例
-    stats = GameeStats(ai_settings)
-
-    # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets) # 创建玩家的输入
-
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
+                        aliens, bullets)
         if stats.game_active:
-            ship.update()   # 飞船的位置将在检测事件后更新屏幕前更新
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)  # 更新使用未消失的子弹的位置
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            # 每次循环时都重绘屏幕
+            ship.update()
+            gf.upadte_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets)
+            # print(len(bullets))
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button)
 
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)    # 更新后的位置绘制新屏幕
 
-run_game() # 初始化游戏并开始主循环
+run_game()
--- ship.py
+++ ship.py
@@ -1,32 +1,34 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
-    
+
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
-        '''初始化飞船并设置初始位置'''
+        """初始化飞船并设置其初始位置"""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
-        # 加载飞船图像并获取外形矩形
-        self.image = pygame.image.load('images/ship.png')   # 返回存储飞船图像
-        self.rect = self.image.get_rect()   # 获取图像属性
-        self.screen_rect = screen.get_rect()    # 存储图像属性
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
         # 将每艘新飞船放在屏幕底部中央
-        self.rect.centerx = self.screen_rect.centerx    # 设置飞船中心的x坐标
-        self.rect.bottom = self.screen_rect.bottom  # 设置飞船下边缘的y坐标
-        
-        # 在飞船的属性cente中存储小数值
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船的属性center中存储小数值
         self.center = float(self.rect.centerx)
 
         # 移动标志
-        self.moving_right = False # 初始化属性
+        self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        '''根据移动标志调整飞船位置'''
-        # 更新飞船的center值，而不是rect
-        if self.moving_right and self.rect.right < self.screen_rect.right:   # 移动标志为true时移动飞船
+        """根据移动标志调整飞船的位置"""
+        # 更新飞船的center值而不是rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
@@ -35,10 +37,9 @@ class Ship():
         self.rect.centerx = self.center
 
     def blitme(self):
-        '''在指定位置绘制飞船'''
-        self.screen.blit(self.image, self.rect) # 指定的位置将图像绘制到屏幕上
-    
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+
     def center_ship(self):
-        '''让飞船在屏幕上居中'''
+        """让飞船在屏幕上居中"""
         self.center = self.screen_rect.centerx
-        
